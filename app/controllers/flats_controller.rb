e4933ed78ffd258877a1428bb1cfe2b863af7114
Aleksejs Brehovs <aleksi.b.tr@gmail.com>
2022-02-16 17:04:03 +0000

    some bootstrap

--- app/controllers/flats_controller.rb
+++ app/controllers/flats_controller.rb
@@ -1,2 +1,26 @@
 class FlatsController < ApplicationController
+  def index
+    @flats = Flat.all
+  end
+
+  def new
+    @flat = Flat.new
+  end
+
+  def create
+    @flat = Flat.new(strong_params)
+    if @flat.save
+      redirect_to flats_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def strong_params
+    params.require(:flat).permit(
+      :name, :address, :description, :price_per_night, :number_of_guests
+    )
+  end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,13 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+(1..4).each do |n|
+  Flat.create!(
+    name: 'Light & Spacious Garden Flat London',
+    address: "#{n} Clifton Gardens London W#{n} 1DT",
+    description: 'A lovely summer feel for this spacious garden flat',
+    price_per_night: 75,
+    number_of_guests: n
+  )
+end
